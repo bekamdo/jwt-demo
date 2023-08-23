e440d9883abe997b9607b2728f6f03b9fb404f01
bekamdo <bekamdo@gmail.com>
2023-08-23 21:45:16 +0300

    first commit

---
+++ src/main/java/com/bekamdo/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.bekamdo.config;
+
+import ch.qos.logback.core.model.IncludeModel;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    private final RsaKeyProperties rsaKey;
+
+    public SecurityConfig(RsaKeyProperties rsaKey) {
+        this.rsaKey = rsaKey;
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager user(){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("bekamdo")
+                        .password("{noop}password")
+                        .authorities("read")
+                        .build()
+        );
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       return http
+               .csrf(csrf -> csrf.disable())
+               .authorizeRequests(auth -> auth.anyRequest().authenticated() )
+               .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+               .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+               .httpBasic(Customizer.withDefaults())
+               .build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withPublicKey(rsaKey.publicKey()).build();
+    }
+    @Bean
+    JwtEncoder jwtEncoder(){
+        JWK jwk = new RSAKey.Builder(rsaKey.publicKey()).privateKey(rsaKey.privateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return  new NimbusJwtEncoder(jwks);
+
+    }
+}
---
+++ src/test/java/com/bekamdo/controller/HomeControllerTest.java
@@ -0,0 +1,7 @@
+package com.bekamdo.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeControllerTest {
+
+}
